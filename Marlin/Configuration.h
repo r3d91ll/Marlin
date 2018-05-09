e12474457b1f5a2a0cddad2a01ac95c28c4883d3
dot-bob <robfrommendon@gmail.com>
2018-05-08 21:31:12 -0600

    Acceleration changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -617,7 +617,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 750, 750, 100, 500 }
+#define DEFAULT_MAX_ACCELERATION      { 800, 800, 100, 10000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -628,8 +628,8 @@
  *   M204 T    Travel Acceleration
  */
 #define DEFAULT_ACCELERATION          800    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  500    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   800    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
@@ -639,8 +639,8 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                  7
-#define DEFAULT_YJERK                  7
+#define DEFAULT_XJERK                  6
+#define DEFAULT_YJERK                  6
 #define DEFAULT_ZJERK                  0.3
 #define DEFAULT_EJERK                  5.0
 
