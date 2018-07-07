d572e58a7adad4537a7274a199c19610d5e4290f
dot-bob <robfrommendon@gmail.com>
2018-07-07 15:42:38 -0600

    Apply MOVE_Z_WHEN_IDLE to example configurations

--- Marlin/src/config/examples/Anet/A6/Configuration_adv.h
+++ Marlin/src/config/examples/Anet/A6/Configuration_adv.h
@@ -744,6 +744,8 @@
   //#define DOUBLECLICK_FOR_Z_BABYSTEPPING // Double-click on the Status Screen for Z Babystepping.
   #define DOUBLECLICK_MAX_INTERVAL 1250 // Maximum interval between clicks, in milliseconds.
                                         // Note: Extra time may be added to mitigate controller latency.
+  #define MOVE_Z_WHEN_IDLE              // Jump to the move Z menu on doubleclick when printer is idle.
+  #define MOVE_Z_IDLE_MULTIPLICATOR 1   // Multiply 1mm by this factor for the move step size.
   //#define BABYSTEP_ZPROBE_GFX_OVERLAY // Enable graphical overlay on Z-offset editor
 #endif
 
